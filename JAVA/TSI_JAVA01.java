c09a8b1ecd8ade351593bc79f6637f2d6785f96f
FOX-Braga <joaogabrielbraaa@gmail.com>
2025-04-05 15:17:03 -0300

    ola mundo em java
    primeiro Ola mundo Em java

---
+++ JAVA/TSI_JAVA01.java
@@ -0,0 +1,9 @@
+package JAVA;
+ import java.util.Scanner;
+ 
+ public class TSI_JAVA01 {
+    public static void main(String[] args) {
+        System.out.println("Hellon world");
+        
+    } 
+ }
