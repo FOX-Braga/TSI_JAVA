d142bbb5ceeda101e496cf959242fa0907e6d85f
Joao Braga <joaogabrielbraaa@gmail.com>
2025-04-23 14:17:43 -0400

    Create switch.java

---
+++ JAVA/switch.java
@@ -0,0 +1,9 @@
+package JAVA;
+import java.util.Scanner;
+
+public class switch {
+    String tipo;
+    int pernas;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("informe a quantidades de patas :")
+}
